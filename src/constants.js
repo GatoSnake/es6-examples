6ae2614a483d9ce3c26c999f1c4fe4ebf6a13b97
Cristhian Won <cristhian.won@gmail.com>
2017-04-01 20:45:17 -0300

    edit gitignore again

--- src/constants.js
+++ src/constants.js
@@ -1,6 +1,6 @@
 export default function es6_constants() {
     console.log(`
-      ************** Constants exampless **************
+      ************** Constants examples **************
       `);
     const PI = 3.141593;
     console.log(`Compare constant PI > 3.0:`, PI > 3.0);
